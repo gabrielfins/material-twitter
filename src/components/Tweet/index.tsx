89f997895e9bdc366bc41536ac27b46cba445d54
Gabriel Figueredo <contato.gabrielfins@gmail.com>
2021-03-07 04:29:56 -0300

    Added hability to load tweets dinamically from a json file and fixed problem with navbar z-index

--- src/components/Tweet/index.tsx
+++ src/components/Tweet/index.tsx
@@ -1,54 +1,54 @@
 import './style.scss';
 import '@gabrielfins/ripple-effect';
-import { FaUserCircle, FaRegComment, FaRetweet, FaHeart, FaLink } from 'react-icons/fa';
+import { FaUserCircle, FaRegComment, FaRetweet, FaRegHeart, FaHeart, FaLink } from 'react-icons/fa';
 import { FiUpload } from 'react-icons/fi';
 import { MdMoreVert, MdMail, MdBookmark, MdPlaylistAdd, MdVolumeOff } from 'react-icons/md';
-import { IoPersonRemoveSharp, IoFlag } from 'react-icons/io5';
+import { IoPersonAddSharp, IoPersonRemoveSharp, IoFlag } from 'react-icons/io5';
 import { AiOutlineStop } from 'react-icons/ai';
 import { ImEmbed2 } from 'react-icons/im';
 import { useState } from 'react';
 
-export default function Tweet() {
+export default function Tweet(props: any) {
     const [isUserDropdownOpen, setUserDropdownOpen] = useState(false);
     const [isOperationsDropdownOpen, setOperationsDropdownOpen] = useState(false);
 
     return (
         <div className="tweet">
-            <div className="tweet-row">
+            <div className="tweet-row align-start">
                 <div className="tweet-column user">
-                    <FaUserCircle className="user-image" size="3.2em" />
+                    {props.userImageUrl != "" ? <img src={ props.userImageUrl } alt=""/> : <FaUserCircle className="user-image" size="3.2em" />}
                 </div>
                 <div className="tweet-column">
                     <div className="tweet-row user-info">
-                        <p className="tweet-user-info"><span className="user-name">Usuário</span><span>@usuario</span><span>• 10h</span></p>
+                        <p className="tweet-user-info"><span className="user-name">{ props.userName }</span><span>@{ props.userUsername }</span><span>• { props.timeFromPosting }</span></p>
                         <div className={`transparent-overlay ${ isUserDropdownOpen ? 'open' : 'close' }`} onClick={ () => { setUserDropdownOpen(false) } } ></div>
                         <div className="dropdown-container">
                             <button className="ripple-effect ripples-theme tweet-icon-button dropdown" onClick={ () => setUserDropdownOpen(!isUserDropdownOpen) }><MdMoreVert size="1.3em" /></button>
                             {isUserDropdownOpen &&
                                 <div className="tweet-dropdown user-actions">
-                                    <button className="ripple-effect"><IoPersonRemoveSharp className="dropdown-icon" size="1.3em" />Deixar de seguir @usuario</button>
-                                    <button className="ripple-effect"><MdPlaylistAdd className="dropdown-icon" size="1.3em" />Adicionar/remover @usuario das Listas</button>
-                                    <button className="ripple-effect"><MdVolumeOff className="dropdown-icon" size="1.3em" />Silenciar @usuario</button>
-                                    <button className="ripple-effect"><AiOutlineStop className="dropdown-icon" size="1.3em" />Bloquear @usuario</button>
+                                    <button className="ripple-effect">{ props.isUserFollowed ? <IoPersonRemoveSharp className="dropdown-icon" size="1.3em" /> : <IoPersonAddSharp className="dropdown-icon" size="1.3em" /> }{` ${props.isUserFollowed ? "Deixar de seguir" : "Seguir"} @${props.userUsername} `}</button>
+                                    <button className="ripple-effect"><MdPlaylistAdd className="dropdown-icon" size="1.3em" />Adicionar/remover @{ props.userUsername } das Listas</button>
+                                    <button className="ripple-effect"><MdVolumeOff className="dropdown-icon" size="1.3em" />Silenciar @{ props.userUsername }</button>
+                                    <button className="ripple-effect"><AiOutlineStop className="dropdown-icon" size="1.3em" />Bloquear @{ props.userUsername }</button>
                                     <button className="ripple-effect"><ImEmbed2 className="dropdown-icon" size="1.3em" />Incorporar Tweet</button>
                                     <button className="ripple-effect"><IoFlag className="dropdown-icon" size="1.3em" />Denunciar Tweet</button>
                                 </div>
                             }
                         </div>
                     </div>
-                    <p className="tweet-content-text">Texto do tweet</p>
-                    <img src="" alt=""/>
+                    <p className="tweet-content-text">{ props.tweetText }</p>
+                    { props.tweetImageUrl != "" && props.tweetImageUrl }
                 </div>
             </div>
             <div className="tweet-row operations">
                 <div className="operation-container">
-                    <button className="ripple-effect ripples-theme tweet-icon-button"><FaRegComment size="1.3em" /></button><span>9</span>
+                    <button className={"ripple-effect ripples-theme tweet-icon-button operation comment"}><FaRegComment size="1.3em" /></button><span>{ props.numberOfComments }</span>
                 </div>
                 <div className="operation-container">
-                    <button className="ripple-effect ripples-green tweet-icon-button retweet"><FaRetweet size="1.3em" /></button><span>18</span>
+                    <button className={`ripple-effect ripples-green tweet-icon-button operation retweet ${ props.isTweetRetweeted && 'retweeted' }`}><FaRetweet size="1.3em" /></button><span>{ props.numberOfRetweets }</span>
                 </div>
                 <div className="operation-container">
-                    <button className="ripple-effect ripples-pink tweet-icon-button like"><FaHeart size="1.3em" /></button><span>814</span>
+                    <button className={`ripple-effect ripples-pink tweet-icon-button operation like ${ props.isTweetLiked && 'liked' }`}>{ props.isTweetLiked ? <FaHeart size="1.3em" /> : <FaRegHeart size="1.3em" /> }</button><span>{ props.numberOfLikes }</span>
                 </div>
                 <div className={`transparent-overlay ${ isOperationsDropdownOpen ? 'open' : 'close' }`} onClick={ () => { setOperationsDropdownOpen(false) } } ></div>
                 <div className="dropdown-container">
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
 import './style.scss';
 import Tweet from '../../components/Tweet';
+import data from '../../data-tweets.json';
 
 export default function Home() {
     return (
@@ -7,9 +8,27 @@ export default function Home() {
             <div className="page-content">
             </div>
             <div className="page-main-content">
-                <Tweet />
-                <div className="divider"></div>
-                <Tweet />
+                {data.tweets.map((data: any) => {
+                    return (
+                        <>
+                            <div className="divider"></div>
+                            <Tweet key={ data.id }
+                                   userImageUrl={ data.userImageUrl }
+                                   userName={ data.userName }
+                                   userUsername={ data.userUsername }
+                                   timeFromPosting={ data.timeFromPosting }
+                                   timePosted={ data.timePosted }
+                                   isUserFollowed={ data.isUserFollowed }
+                                   tweetText={ data.tweetText }
+                                   tweetImageUrl={ data.tweetImageUrl }
+                                   numberOfComments={ data.numberOfComments }
+                                   numberOfRetweets={ data.numberOfRetweets }
+                                   numberOfLikes={ data.numberOfLikes }
+                                   isTweetRetweeted={ data.isTweetRetweeted }
+                                   isTweetLiked={ data.isTweetLiked } />
+                        </>
+                    );
+                })}
             </div>
             <div className="page-content-left">
             </div>  
